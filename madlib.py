6745a3fb0d5572d46f45a33fa2974c2bc3b27ee3
TJVictor2002 <tja50556@gmail.com>
2026-07-07 20:32:03 -0400

    Update madlib.py - add while True and option to generate more stories

--- madlib.py
+++ madlib.py
@@ -1,18 +1,23 @@
 def main():
     prompts = ["adjective", "noun", "verb (past tense)", "animal"]
-    words = {}
 
-    for word_type in prompts:
-        words[word_type] = input(f"Give me a {word_type}: ").strip()
+    while True:
+        words = {}
+        for word_type in prompts:
+            words[word_type] = input(f"Give me a {word_type}: ").strip()
 
-    story = f"""
+        story = f"""
 Once upon a time, there was a {words["adjective"]} {words["noun"]}.
 One day, it {words["verb (past tense)"]} all the way to the zoo,
 where it made friends with a very confused {words["animal"]}.
 The end.
 """
 
-    print(story)
+        print(story)
+
+        again = input("Generate another story? (y/n): ").strip().lower()
+        if again != "y":
+            break
 
 
 main()
